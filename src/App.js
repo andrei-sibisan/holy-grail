7e918165c77584d6f0d786f2c8f834789d75ce55
andrada <andrada.nistor@gmail.com>
2022-09-03 12:50:13 +0300

    update greeting component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import Counter from "./components/Counter";
 import Greeting from "./components/Greeting";
 
 export default function App() {
-  const name = "Andrada";
-  const age = 37;
+
   return (
     <>
       <Counter />
       <br />
-      <Greeting nume={name} varsta={age} />
+      <Greeting />
     </>
   );
 }
--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,4 +1,10 @@
-export default function Greeting(props) {
-  const { nume, varsta } = props;
-  return `Greetings, ${nume}, ${varsta} years old!`;
+import * as React from "react";
+export default function Greeting() {
+  const [name, setName] = React.useState("");
+  return (
+    <>
+      <input onChange={(text) => setName(text.target.value)} />
+      <span>Salut, {name}</span>
+    </>
+  );
 }
