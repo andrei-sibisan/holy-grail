fc1bd55b9d326d112d684ef9f97afbb23d2b5ae2
andrada <andrada.nistor@gmail.com>
2022-09-04 10:17:58 +0300

    modified app

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ import * as React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./styles.css";
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import Main from "./components/Main";
 import Home from "./views/Home";
 
 export default function App() {
