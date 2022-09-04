f7bb8668cdf699fb1ddae99f6aa6a7971e0757d9
andrada <andrada.nistor@gmail.com>
2022-09-04 09:43:18 +0300

    setup react router library

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import * as React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./styles.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 export default function App() {
-  return (
+  const homeView = (
     <div className="grid-container">
       <Header />
       <Main />
       <Footer />
     </div>
+  );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={homeView} />
+        <Route path="contact" element={homeView} />
+      </Routes>
+    </BrowserRouter>
   );
 }
